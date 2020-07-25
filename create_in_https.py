d14114477f8ba35a6b1a991e74a3d826208d2f1d
barry-gallagher <barry.gallagher@noaa.gov>
2020-07-25 14:56:00 -0400

    fisrt commit to rebase

--- create_in_https.py
+++ create_in_https.py
@@ -3,4 +3,5 @@
 # made this file make an edit directly on master and added in pycharm
 # edit in master to allow a rebase in pycharm
 # then commit in mod in pycharm brnch pycharm to master
+# sourcetee local change to rebase onto master
 # add a line in sourcetree - not sure if this will create a conflcit
