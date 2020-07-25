5e27eda39f5d0086c6ac6004b36e97f3915a4fa1
barry-gallagher <barry.gallagher@noaa.gov>
2020-07-25 14:24:18 -0400

    edit for rebasing in pycharm

--- create_in_https.py
+++ create_in_https.py
@@ -1,3 +1,4 @@
+# added in pycharm to rebase.  may be conflict?
 # made this file make an edit directly on master and added in pycharm
 # edit in master to allow a rebase in pycharm
 # then commit in mod in pycharm brnch pycharm to master
