f5d728fbbf79e38c07d44f8c3d3e77e31d37578a
barry-gallagher <barry.gallagher@noaa.gov>
2020-07-25 14:53:27 -0400

    master change for rebase2

--- create_in_https.py
+++ create_in_https.py
@@ -1,4 +1,5 @@
 # added in pycharm to rebase.  may be conflict?
+# This is into master to make sourcetree rebase a 2nd time
 # made this file make an edit directly on master and added in pycharm
 # edit in master to allow a rebase in pycharm
 # then commit in mod in pycharm brnch pycharm to master
